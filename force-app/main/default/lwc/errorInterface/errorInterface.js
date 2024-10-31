5b75865b2dbc8ba258c245fcd744683022e0af71
DCX-Team-knowledge <150586314+DCX-Team-knowledge@users.noreply.github.com>
2024-10-31 12:04:49 +0100

    Update errorInterface.js

--- force-app/main/default/lwc/errorInterface/errorInterface.js
+++ force-app/main/default/lwc/errorInterface/errorInterface.js
@@ -1,1 +1,10 @@
 
+import { LightningElement, api } from 'lwc'
+
+export default class CrmCpErrorInterface extends LightningElement {
+  @api errorMessageBody
+
+  get errorMessageHeader() {
+    return 'Error retrieving data'
+  }
+}
