59ecee4ee036be6970c5ef3d1e6d0e80a82172ff
DCX-Team-knowledge <150586314+DCX-Team-knowledge@users.noreply.github.com>
2024-10-31 11:43:48 +0100

    Update CommonAutocomplete.js

--- force-app/main/default/lwc/CommonAutocomplete/CommonAutocomplete.js
+++ force-app/main/default/lwc/CommonAutocomplete/CommonAutocomplete.js
@@ -1,1 +1,37 @@
 
+import { LightningElement, api } from 'lwc'
+
+export default class CommonAutocomplete extends LightningElement {
+  @api name = ''
+  @api value = ''
+  @api placeholder = ''
+  @api label = ''
+  @api options
+  @api disabled
+  @api required
+
+  initialized = false
+
+  renderedCallback() {
+    if (this.initialized) {
+      return
+    }
+    this.initialized = true
+
+    let listId = this.template.querySelector('datalist').id
+    this.template.querySelector('input').setAttribute('list', listId)
+  }
+
+  get optionValues() {
+    return this.options || []
+  }
+
+  handleChange({ target: { value } }) {
+    this.dispatchEvent(
+      new CustomEvent('change', {
+        bubbles: false,
+        detail: { value: value, target: this.name },
+      })
+    )
+  }
+}
