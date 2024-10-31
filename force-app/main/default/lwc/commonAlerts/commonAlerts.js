5550c4b38bb1fc3cddd774abcb41f298910f8995
DCX-Team-knowledge <150586314+DCX-Team-knowledge@users.noreply.github.com>
2024-10-31 11:40:12 +0100

    Update commonAlerts.js

--- force-app/main/default/lwc/commonAlerts/commonAlerts.js
+++ force-app/main/default/lwc/commonAlerts/commonAlerts.js
@@ -1,1 +1,26 @@
+import { LightningElement, api } from 'lwc'
 
+export default class CommonAlerts extends LightningElement {
+  _parsedAlerts = null
+
+  @api get alerts() {
+    return this._parsedAlerts
+  }
+
+  set alerts(alerts) {
+    if (!alerts) {
+      return
+    }
+
+    this._parsedAlerts = alerts.map((alert) => ({
+      key: `${alert.alertType}-${alert.message}`,
+      message: alert.message,
+      classes: `slds-var-m-bottom_xx-small slds-notify slds-notify_alert slds-alert_${
+        alert.alertType === 'critical' ? 'error' : alert.alertType
+      }`,
+      icon: `/apexpages/slds/latest/assets/icons/utility-sprite/svg/symbols.svg#${
+        alert.alertType === 'critical' ? 'error' : alert.alertType
+      }`,
+    }))
+  }
+}
